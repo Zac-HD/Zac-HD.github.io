2747c73178342924c8853739f293d828628b9a7b
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2022-08-10 02:09:21 -0700

    Fix spinner, faster repaint

--- ghostwriter/hydemo.js
+++ ghostwriter/hydemo.js
@@ -23,6 +23,8 @@ _setup().then((hydemoMod) => {
   document.getElementById("pytest-spinner").style.visibility = "hidden";
 });
 
+const allow_time_for_paint = () => new Promise(resolve => setTimeout(resolve, 50));
+
 async function write_tests() {
   spinner = document.getElementById("ghostwriter-spinner");
   spinner.style.visibility = "visible";
@@ -32,12 +34,14 @@ async function write_tests() {
   style = document.querySelector("input[name=style]:checked").value;
   code_div.textContent = await hydemoMod.write_a_test(func_name, writer, style);
   spinner.style.visibility = "hidden";
+  await allow_time_for_paint()
   run_tests();
 }
 
-function run_tests() {
+async function run_tests() {
   spinner = document.getElementById("pytest-spinner");
   spinner.style.visibility = "visible";
+  await allow_time_for_paint()
   code_div = document.getElementById("ghostwriter-output");
   code_div = document.getElementById("ghostwriter-output");
   source_code = hydemoMod.format_code(code_div.textContent);
