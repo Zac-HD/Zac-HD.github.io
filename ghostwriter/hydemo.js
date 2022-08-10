005a2c345ce0dd6c791c1821ceb387233d7659f5
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2022-08-10 02:11:35 -0700

    Function as a query param

--- ghostwriter/hydemo.js
+++ ghostwriter/hydemo.js
@@ -1,4 +1,10 @@
 async function _setup() {
+  document.addEventListener('DOMContentLoaded', (event) => {
+    let params = new URLSearchParams(window.location.search);
+    if (params.has("q")) {document.getElementById("function_names").setAttribute('value', params.get("q"))};
+    //if (params.has("writer") && params.get("writer") != "magic") {};
+    //if (params.has("style") && params.get("style") != "pytest") {};
+  })
   const hydemoPromise = fetch("hydemo.zip").then((resp) => resp.arrayBuffer());
   let pyodide = await loadPyodide();
   let micropip_promise = pyodide.loadPackage("micropip");
@@ -21,6 +27,8 @@ _setup().then((hydemoMod) => {
   });
   document.getElementById("ghostwriter-spinner").style.visibility = "hidden";
   document.getElementById("pytest-spinner").style.visibility = "hidden";
+  let params = new URLSearchParams(window.location.search);
+  if (params.has("q")) {write_tests();};
 });
 
 const allow_time_for_paint = () => new Promise(resolve => setTimeout(resolve, 50));
@@ -32,6 +40,14 @@ async function write_tests() {
   func_name = document.getElementById("function_names").value;
   writer = document.querySelector("input[name=writer]:checked").value;
   style = document.querySelector("input[name=style]:checked").value;
+
+  let searchParams = new URLSearchParams();
+  searchParams.set("q", func_name);
+  //if (writer != "magic") { searchParams.set("writer", writer) };
+  //if (style != "pytest") { searchParams.set("style", style) };
+  const url = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + searchParams.toString();
+  window.history.replaceState({path: url}, "", url)
+
   code_div.textContent = await hydemoMod.write_a_test(func_name, writer, style);
   spinner.style.visibility = "hidden";
   await allow_time_for_paint()
